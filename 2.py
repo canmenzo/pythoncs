a597f65561ccda83d43c411e727702cee447bd68
Menzo <canozmen@mail.com>
2024-03-13 13:04:40 -0600

    Update 2.py

--- 2.py
+++ 2.py
@@ -1,7 +1,9 @@
+### IMPORTS FUNCTIONS FOR REGEX (FOR HASH), IPADDRESS VALIDATION, AND WHOIS (DOMAIN) VALIDATION
 import re
 import ipaddress
 import whois
 
+### CHECK FUNCTION FOR ALL 3 INSTANCES
 def check(x):
     if checkHash(x) == True:
         return print(f"https://www.virustotal.com/gui/file/{x}")
@@ -12,6 +14,7 @@ def check(x):
     else:
         print("You have entered not a correct value.")
 
+### CHECK FUNCTION FOR HASH, REGEX AND RETURNS THE CORRECT OUTPUT
 def checkHash(x):
     MD5 = "^[a-fA-F0-9]{32}$" ### md5 test: 098f6bcd4621d373cade4e832627b4f6
     SHA1 = "^[a-fA-F0-9]{40}$" ### sha1 test: a94a8fe5ccb19ba61c4c0873d391e987982fbbd3
@@ -29,6 +32,7 @@ def checkHash(x):
     else:
         return False
 
+### CHECKS THE IP ADDRESS FUNCTION
 def checkIP(x):
     ### ip test: 163.116.145.31
     try:
@@ -38,6 +42,7 @@ def checkIP(x):
     except ValueError:
         return False
 
+### CHECK THE DOMAIN FUNCTION BY WHOIS IF ITS REGISTERED
 def checkDomain(x):
     try:
       domain_info = whois.whois(x)
@@ -47,7 +52,8 @@ def checkDomain(x):
       print("The value you have entered is not a registered domain.")
       return False
 
+### PRINTS main lines
 print("Welcome to the OSINT generator!")
 value = input("\nPlease enter your malicious entity (i.e. IP Address, Hash Value (SHA1, SHA256, MD5), Domain):")
-
+### starts the main functions
 check(value)
